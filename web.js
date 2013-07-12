68e3a0d873a9bf2f6c246193c81d935e614e9df8
Ubuntu <ubuntu@ip-172-31-3-158.us-west-2.compute.internal>
2013-07-12 05:00:00 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,13 +1,15 @@
+//Homework #3
+//Get neccessary modules
 var express = require('express');
 var fs = require('fs')
 var app = express.createServer(express.logger());
-
+//Determine Output
 app.get('/', function(request, response) {
   var infile ="index.html";
   var out = fs.readFileSync(infile)
   response.send( out );
 });
-
+//Routing Output
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
